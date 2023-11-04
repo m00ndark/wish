d8982af420ba915cae8d6647f2fe6a0bb21056f8
Mattias Wijkström <mattias.wijkstrom@gmail.com>
2023-11-04 21:27:41 +0100

    Change path to connection file

--- common.php
+++ common.php
@@ -24,7 +24,7 @@ function dbConnect()
 	}
 	try
 	{
-		$connectionFileContent = file_get_contents(__DIR__ . "/../../$connectionFile");
+		$connectionFileContent = file_get_contents(__DIR__ . "/../../../$connectionFile");
 		$connectionInfo = json_decode($connectionFileContent);
 	}
 	catch (Exception $ex)
